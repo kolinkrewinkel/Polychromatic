6fb10d5caf65b73315cd21ec23a6164cf30424b3
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2014-03-09 16:54:40 -0700

    Initial Commit

---
+++ Semantique/Semantique.h
@@ -0,0 +1,13 @@
+//
+//  Semantique.h
+//  Semantique
+//
+//  Created by Kolin Krewinkel on 3/9/14.
+//  Copyright (c) 2014 Kolin Krewinkel. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface Semantique : NSObject
+
+@end

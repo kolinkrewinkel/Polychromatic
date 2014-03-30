b26e148f650e44d53fbe986dea6563b756162ce5
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2014-03-30 12:51:57 -0700

    Add/revise headers more.

--- Semantique/DVT Classes/DVTInterfaces.h
+++ Semantique/DVT Classes/DVTInterfaces.h
@@ -1,11 +1,11 @@
 //
-//  CPXcodeInterfaces.h
-//  CodePilot
+//  DVTInterfaces.h
+//  Semantique
 //
-//  Created by Zbigniew Sobiecki on 3/24/10.
-//  Copyright 2010 Macoscope. All rights reserved.
+//  Created by Kolin Krewinkel on 3/30/14.
+//  Copyright 2014 Apple Inc. All rights reserved.
 //
-//  Take from CodePilot: https://github.com/macoscope/CodePilot
+//  Sourced from class-dump. Some borrowed from CodePilot for quick compilation.
 //
 
 #import <Cocoa/Cocoa.h>
@@ -801,5 +801,157 @@ extern NSString *IDEEditorDocumentDidChangeNotification;
 // FIXME: Workaround for NSObject+YOLO buggyness
 -(NSColor*)yl_colorAtCharacterIndex:(unsigned long long)charIndex effectiveRange:(NSRangePointer)range context:(id)context;
 - (id<DVTSourceLanguageSourceModelService>)sourceModelItemAtCharacterIndex:(unsigned long long)arg1; //DVTSourceTextStorage in Xcode 5, DVTSourceLanguageSourceModelService protocol in Xcode 5.1
+
+@end
+
+@class DVTCustomDataSpecifier, DVTPointerArray, DVTStackBacktrace, NSColor, NSFont, NSImage, NSString, NSURL;
+
+@interface DVTFontAndColorTheme : NSObject <DVTPreferenceSet>
+{
+    NSString *_name;
+    NSImage *_image;
+    NSURL *_dataURL;
+    DVTCustomDataSpecifier *_customDataSpecifier;
+    NSColor *_sourceTextBackgroundColor;
+    NSColor *_sourceTextSidebarBackgroundColor;
+    NSColor *_sourceTextSidebarEdgeColor;
+    NSColor *_sourceTextSidebarNumbersColor;
+    NSColor *_sourceTextFoldbarBackgroundColor;
+    NSColor *_sourceTextSelectionColor;
+    NSColor *_sourceTextSecondarySelectionColor;
+    NSColor *_sourceTextInsertionPointColor;
+    NSColor *_sourceTextInvisiblesColor;
+    NSColor *_sourceTextBlockDimBackgroundColor;
+    NSColor *_sourceTextTokenizedBorderColor;
+    NSColor *_sourceTextTokenizedBackgroundColor;
+    NSColor *_sourceTextTokenizedBorderSelectedColor;
+    NSColor *_sourceTextTokenizedBackgroundSelectedColor;
+    NSColor *_consoleTextBackgroundColor;
+    NSColor *_consoleTextSelectionColor;
+    NSColor *_consoleTextSecondarySelectionColor;
+    NSColor *_consoleTextInsertionPointColor;
+    NSColor *_consoleDebuggerPromptTextColor;
+    NSColor *_consoleDebuggerInputTextColor;
+    NSColor *_consoleDebuggerOutputTextColor;
+    NSColor *_consoleExecutableInputTextColor;
+    NSColor *_consoleExecutableOutputTextColor;
+    NSFont *_consoleDebuggerPromptTextFont;
+    NSFont *_consoleDebuggerInputTextFont;
+    NSFont *_consoleDebuggerOutputTextFont;
+    NSFont *_consoleExecutableInputTextFont;
+    NSFont *_consoleExecutableOutputTextFont;
+    NSColor *_debuggerInstructionPointerColor;
+    NSColor *_sourcePlainTextColor;
+    NSFont *_sourcePlainTextFont;
+    DVTPointerArray *_syntaxColorsByNodeType;
+    DVTPointerArray *_syntaxFontsByNodeType;
+    NSColor *_sourceTextCompletionPreviewColor;
+    BOOL _builtIn;
+    BOOL _loadedData;
+    BOOL _contentNeedsSaving;
+    BOOL _hasMultipleSourceTextFonts;
+}
+
++ (id)_defaultSourceCodeFont;
++ (id)keyPathsForValuesAffectingConsoleTextSecondarySelectionColor;
++ (id)keyPathsForValuesAffectingSourceTextSecondarySelectionColor;
++ (id)titleForNewPreferenceSetFromTemplate;
++ (id)preferenceSetsListHeader;
++ (id)preferenceSetsFileExtension;
++ (id)defaultKeyForExcludedBuiltInPreferenceSets;
++ (id)defaultKeyForCurrentPreferenceSet;
++ (id)builtInPreferenceSetsDirectoryURL;
++ (id)systemPreferenceSet;
++ (id)preferenceSetGroupingName;
++ (id)_nodeTypesIncludedInPreferences;
++ (id)_stringRepresentationOfFont:(id)arg1;
++ (id)_fontWithName:(id)arg1 size:(double)arg2;
++ (id)currentTheme;
++ (id)preferenceSetsManager;
++ (void)initialize;
+@property(readonly) BOOL loadedData; // @synthesize loadedData=_loadedData;
+@property(readonly) DVTPointerArray *syntaxFontsByNodeType; // @synthesize syntaxFontsByNodeType=_syntaxFontsByNodeType;
+@property(readonly) DVTPointerArray *syntaxColorsByNodeType; // @synthesize syntaxColorsByNodeType=_syntaxColorsByNodeType;
+@property(nonatomic) BOOL hasMultipleSourceTextFonts; // @synthesize hasMultipleSourceTextFonts=_hasMultipleSourceTextFonts;
+@property BOOL contentNeedsSaving; // @synthesize contentNeedsSaving=_contentNeedsSaving;
+@property(retain) DVTCustomDataSpecifier *customDataSpecifier; // @synthesize customDataSpecifier=_customDataSpecifier;
+@property(readonly, getter=isBuiltIn) BOOL builtIn; // @synthesize builtIn=_builtIn;
+@property(retain) NSImage *image; // @synthesize image=_image;
+@property(copy) NSString *name; // @synthesize name=_name;
+- (void)setFont:(id)arg1 forNodeTypes:(id)arg2;
+- (void)setColor:(id)arg1 forNodeTypes:(id)arg2;
+- (void)_setColorOrFont:(id)arg1 forNodeTypes:(id)arg2;
+- (id)fontForNodeType:(short)arg1;
+- (id)colorForNodeType:(short)arg1;
+@property(readonly) NSFont *sourcePlainTextFont;
+@property(readonly) NSColor *sourcePlainTextColor;
+- (void)setDebuggerInstructionPointerColor:(id)arg1;
+- (void)setConsoleExecutableOutputTextFont:(id)arg1;
+- (void)setConsoleExecutableInputTextFont:(id)arg1;
+- (void)setConsoleDebuggerOutputTextFont:(id)arg1;
+- (void)setConsoleDebuggerInputTextFont:(id)arg1;
+- (void)setConsoleDebuggerPromptTextFont:(id)arg1;
+- (void)setConsoleExecutableOutputTextColor:(id)arg1;
+- (void)setConsoleExecutableInputTextColor:(id)arg1;
+- (void)setConsoleDebuggerOutputTextColor:(id)arg1;
+- (void)setConsoleDebuggerInputTextColor:(id)arg1;
+- (void)setConsoleDebuggerPromptTextColor:(id)arg1;
+- (void)primitiveSetConsoleDebuggerPromptTextColor:(id)arg1;
+- (void)setConsoleTextInsertionPointColor:(id)arg1;
+- (void)setConsoleTextSelectionColor:(id)arg1;
+- (void)setConsoleTextBackgroundColor:(id)arg1;
+- (void)setSourceTextInvisiblesColor:(id)arg1;
+- (void)setSourceTextInsertionPointColor:(id)arg1;
+- (void)setSourceTextSelectionColor:(id)arg1;
+- (void)setSourceTextBackgroundColor:(id)arg1;
+- (void)_setColorOrFont:(id)arg1 forKey:(id)arg2 colorOrFontivar:(id *)arg3;
+@property(readonly) NSColor *debuggerInstructionPointerColor;
+@property(readonly) NSFont *consoleExecutableOutputTextFont;
+@property(readonly) NSFont *consoleExecutableInputTextFont;
+@property(readonly) NSFont *consoleDebuggerOutputTextFont;
+@property(readonly) NSFont *consoleDebuggerInputTextFont;
+@property(readonly) NSFont *consoleDebuggerPromptTextFont;
+@property(readonly) NSColor *consoleExecutableOutputTextColor;
+@property(readonly) NSColor *consoleExecutableInputTextColor;
+@property(readonly) NSColor *consoleDebuggerOutputTextColor;
+@property(readonly) NSColor *consoleDebuggerInputTextColor;
+@property(readonly) NSColor *consoleDebuggerPromptTextColor;
+@property(readonly) NSColor *consoleTextInsertionPointColor;
+@property(readonly) NSColor *consoleTextSecondarySelectionColor;
+@property(readonly) NSColor *consoleTextSelectionColor;
+@property(readonly) NSColor *consoleTextBackgroundColor;
+@property(readonly) NSColor *sourceTextTokenizedBackgroundSelectedColor;
+@property(readonly) NSColor *sourceTextTokenizedBorderSelectedColor;
+@property(readonly) NSColor *sourceTextTokenizedBackgroundColor;
+@property(readonly) NSColor *sourceTextTokenizedBorderColor;
+@property(readonly) NSColor *sourceTextLinkColor;
+@property(readonly) NSColor *sourceTextCompletionPreviewColor;
+@property(readonly) NSColor *sourceTextBlockDimBackgroundColor;
+@property(readonly) NSColor *sourceTextInvisiblesColor;
+@property(readonly) NSColor *sourceTextInsertionPointColor;
+@property(readonly) NSColor *sourceTextSecondarySelectionColor;
+@property(readonly) NSColor *sourceTextSelectionColor;
+@property(readonly) NSColor *sourceTextFoldbarBackgroundColor;
+@property(readonly) NSColor *sourceTextSidebarNumbersColor;
+@property(readonly) NSColor *sourceTextSidebarEdgeColor;
+@property(readonly) NSColor *sourceTextSidebarBackgroundColor;
+@property(readonly) NSColor *sourceTextBackgroundColor;
+- (id)description;
+@property(readonly) NSString *localizedName;
+- (void)_updateHasMultipleSourceTextFonts;
+- (void)_updateDerivedColors;
+- (BOOL)_loadFontsAndColors;
+- (id)dataRepresentationWithError:(id *)arg1;
+- (void)primitiveInvalidate;
+- (id)initWithCustomDataSpecifier:(id)arg1 basePreferenceSet:(id)arg2;
+- (id)initWithName:(id)arg1 dataURL:(id)arg2;
+- (id)_initWithName:(id)arg1 syntaxColorsByNodeType:(id)arg2 syntaxFontsByNodeType:(id)arg3;
+- (void)_themeCommonInit;
+- (id)init;
+
+// Remaining properties
+@property(retain) DVTStackBacktrace *creationBacktrace;
+@property(readonly) DVTStackBacktrace *invalidationBacktrace;
+@property(readonly, nonatomic, getter=isValid) BOOL valid;
 
 @end

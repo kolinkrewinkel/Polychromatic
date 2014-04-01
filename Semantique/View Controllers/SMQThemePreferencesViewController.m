0e269c5004adcfa98d2428d849150d4e7596e3d4
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2014-04-01 11:49:14 -0700

    Begin the layer visualization and working towards a preferences view. To sketches...

--- Semantique/View Controllers/SMQThemePreferencesViewController.m
+++ Semantique/View Controllers/SMQThemePreferencesViewController.m
@@ -39,6 +39,18 @@
 
 }
 
+#pragma mark - View Management
+
+- (void)setupContentView
+{
+    NSView *contentView = [self.view valueForKey:@"contentView"];
+
+    NSView *tableView = [[NSView alloc] initWithFrame:NSMakeRect(0.f, 0.f, 100.f, 100.f)];
+    tableView.wantsLayer = YES;
+    tableView.layer.backgroundColor = [[NSColor redColor] colorWithAlphaComponent:0.1f].CGColor;
+    [contentView addSubview:tableView];
+}
+
 #pragma mark - Setters
 
 - (void)setRepresentedExtension:(DVTExtension *)extension
@@ -54,11 +66,22 @@
     [super setView:view];
 
     self.view.wantsLayer = YES;
+//    self.view.bounds = CGRectMake(0.f, 0.f, 1.f, 1.f);
+
+    self.view.layer.backgroundColor = [[NSColor redColor] colorWithAlphaComponent:0.1f].CGColor;
+
+//    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeWidth relatedBy:0 toItem:self.view.superview attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
+//    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeHeight relatedBy:0 toItem:self.view.superview attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
+//
+//    [self.view addConstraints:@[width, height]];
 
     NSView *contentView = [[NSView alloc] initWithFrame:self.view.bounds];
     contentView.wantsLayer = YES;
+    contentView.layer.backgroundColor = [[NSColor redColor] colorWithAlphaComponent:0.1f].CGColor;
 
     [self.view setValue:contentView forKey:@"contentView"];
+
+    [self setupContentView];
 }
 
 #pragma mark - Sizing

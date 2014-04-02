5190b59334d3e5d78b1a7dbf09cdb842aac1d779
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2014-04-02 12:02:46 -0700

    Remove needless code.

--- Semantique/View Controllers/SMQThemePreferencesViewController.m
+++ Semantique/View Controllers/SMQThemePreferencesViewController.m
@@ -39,10 +39,6 @@
 - (void)loadView
 {
     [super loadView];
-
-    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"NSConstraintBasedLayoutVisualizeMutuallyExclusiveConstraints"];
-
-    self.view.layer.backgroundColor = [NSColor redColor].CGColor;
 }
 
 #pragma mark - DVTViewController
@@ -57,18 +53,7 @@
 - (void)setupContentView
 {
     NSView *contentView = [self.view valueForKey:@"contentView"];
-    [contentView.layer setBackgroundColor:[NSColor greenColor].CGColor];
     contentView.frame = contentView.superview.frame;
-
-    NSLog(@"%@", NSStringFromRect(contentView.frame));
-    NSLog(@"%@", NSStringFromRect(contentView.bounds));
-    NSLog(@"%@", NSStringFromRect(self.view.frame));
-    NSLog(@"%@", NSStringFromRect(self.view.bounds));
-
-//    NSTableView *tableView = [[NSTableView alloc] initWithFrame:NSMakeRect(20.f, 20.f, 144.f, 144.f)];
-//    tableView.autoresizingMask = NSViewHeightSizable;
-//
-//    [self.view addSubview:tableView];
 }
 
 #pragma mark - Setters
@@ -87,24 +72,7 @@
 
     self.view.wantsLayer = YES;
 
-    self.view.layer.backgroundColor = [[NSColor redColor] colorWithAlphaComponent:0.1f].CGColor;
-
-//    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeWidth relatedBy:0 toItem:self.view.superview attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
-//    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeHeight relatedBy:0 toItem:self.view.superview attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
-//
-//    [self.view addConstraints:@[width, height]];
-
-//    NSView *contentView = [[NSView alloc] initWithFrame:self.view.bounds];
-//    contentView.wantsLayer = YES;
-//    contentView.layer.backgroundColor = [[NSColor redColor] colorWithAlphaComponent:0.1f].CGColor;
-//
-//    [self.view setValue:contentView forKey:@"contentView"];
-
     [self setupContentView];
-
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        NSLog(@"%@", NSStringFromRect(self.view.frame));
-    });
 }
 
 #pragma mark - Sizing

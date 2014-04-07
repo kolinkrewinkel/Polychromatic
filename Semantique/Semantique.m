78addf25c9f85cb2e63e1238548ca1b1f0ba335c
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2014-04-06 17:24:07 -0700

    Finish adding copying functionality on first-run and add menu item.

--- Semantique/Semantique.m
+++ Semantique/Semantique.m
@@ -43,14 +43,64 @@
     return [self sharedPluginWithBundle:nil];
 }
 
-- (id)initWithBundle:(NSBundle *)plugin
+- (id)initWithBundle:(NSBundle *)bundle
 {
     if ((self = [super init]))
     {
-        self.bundle = plugin;
+        self.bundle = bundle;
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSMenuItem *editorMenuItem = [[NSApp mainMenu] itemWithTitle:@"Xcode"];
+
+            NSUInteger startingIndex = 7;
+            [[editorMenuItem submenu] insertItem:[NSMenuItem separatorItem] atIndex:startingIndex];
+
+            NSMenuItem *installItem = [[NSMenuItem alloc] initWithTitle:@"Install Semantique Templates" action:@selector(showInstallWindow:) keyEquivalent:@"I"];
+            installItem.target = self;
+            [[editorMenuItem submenu] insertItem:installItem atIndex:startingIndex + 1];
+
+            [[editorMenuItem submenu] insertItem:[NSMenuItem separatorItem] atIndex:startingIndex + 2];
+        });
+
+        BOOL hasCompletedFirstRun = [[NSUserDefaults standardUserDefaults] boolForKey:@"SMQHasCompletedFirstRun"];
+        if (!hasCompletedFirstRun)
+        {
+            [self showInstallWindow:self];
+            [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"SMQHasCompletedFirstRun"];
+        }
     }
 
     return self;
+}
+
+- (void)showInstallWindow:(id)sender
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Semantique" defaultButton:@"Install Themes" alternateButton:@"Dismiss" otherButton:nil informativeTextWithFormat:@"This plugin is intended for use with monochromatic colors for all types except vibrant variables (local and instance variables, as well as properties.)\n\nTasteful sample themes are provided to demo the concept. Installing them as a basis is recommended."];
+    [alert setAlertStyle:NSInformationalAlertStyle];
+
+    if ([alert runModal] == 1)
+    {
+        __block NSError *error = nil;
+        NSString *basePath = [self.bundle resourcePath];
+        NSArray *themes = [[[NSFileManager defaultManager] contentsOfDirectoryAtPath:basePath error:&error] filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self.absoluteString ENDSWITH '.dvtcolortheme'"]];
+
+        if (error)
+        {
+            NSLog(@"nsfilemanager error: %@", error);
+
+            return;
+        }
+
+        [themes enumerateObjectsUsingBlock:^(NSString *themePath, NSUInteger idx, BOOL *stop) {
+            NSString *replacementName = [themePath.lastPathComponent stringByReplacingOccurrencesOfString:@".dvtcolortheme" withString:@" (Semantique).dvtcolortheme"];
+            [[NSFileManager defaultManager] copyItemAtPath:[NSString stringWithFormat:@"%@/%@", basePath, themePath] toPath:[NSString stringWithFormat:@"%@/Library/Developer/Xcode/UserData/FontAndColorThemes/%@", NSHomeDirectory(), replacementName] error:&error];
+        }];
+    }
+}
+
+- (BOOL)validateMenuItem:(NSMenuItem *)menuItem
+{
+    return YES;
 }
 
 @end

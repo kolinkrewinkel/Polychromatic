776d646c352807d23cf9818fec445a3ac31da913
Michael Conrads <michael.conrads@quartett-mobile.de>
2015-03-19 22:41:42 +0100

    Switched hash from NSString-hash to Fowler–Noll–Vo hash (@McZonk found this hash)

--- Polychromatic/Utilities/PLYVariableManager.m
+++ Polychromatic/Utilities/PLYVariableManager.m
@@ -71,13 +71,26 @@ static NSString *const IDEIndexDidIndexWorkspaceNotification = @"IDEIndexDidInde
         [variables sortUsingDescriptors:@[[[NSSortDescriptor alloc] initWithKey:@"self" ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)]]];
     }
     
-    NSUInteger numberOfDifferentColors = 1000;
-    NSUInteger shortHashValue = [variable hash] % numberOfDifferentColors;
-
+    NSUInteger numberOfDifferentColors = 10000;
+    NSUInteger shortHashValue = [self ply_FNV1Hash:variable] % numberOfDifferentColors;
     CGFloat hueValue = (CGFloat)shortHashValue/(CGFloat)numberOfDifferentColors;
     
     return [NSColor colorWithCalibratedHue:hueValue saturation:[[DVTFontAndColorTheme currentTheme] ply_saturation] brightness:[[DVTFontAndColorTheme currentTheme] ply_brightness] alpha:1.f];
 }
+
+
+- (uint64_t)ply_FNV1Hash:(NSString *)stringToHash
+{
+    // http://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
+    const uint8_t *bytes = (uint8_t *)stringToHash.UTF8String;
+    uint64_t hash = 14695981039346656037ULL;
+    for(uint8_t byte = *bytes; byte != '\0'; byte = *(++bytes))
+    {
+        hash *= 1099511628211ULL;
+        hash ^= byte;
+    }
+     return hash;
+ }
 
 - (void)indexDidIndexWorkspaceNotification:(NSNotification *)notification
 {

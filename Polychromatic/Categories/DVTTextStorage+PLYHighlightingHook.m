ed7e64f9765ddfb7a3a89d0f0e2e40bb7597a05e
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-10-27 10:54:14 -0700

    Refactor PLYHighlightingHook

--- Polychromatic/Categories/DVTTextStorage+PLYHighlightingHook.m
+++ Polychromatic/Categories/DVTTextStorage+PLYHighlightingHook.m
@@ -13,7 +13,7 @@
 #import "PLYVariableManager.h"
 #import "DVTSourceModelItem+PLYIdentification.h"
 
-static IMP originalColorAtCharacterIndexImplementation;
+static IMP originalColorAtIndexImp;
 
 @implementation DVTTextStorage (PLYHighlightingHook)
 
@@ -21,11 +21,11 @@ static IMP originalColorAtCharacterIndexImplementation;
 
 + (void)load
 {
-    originalColorAtCharacterIndexImplementation = PLYPoseSwizzle([DVTTextStorage class],
-                                                                 @selector(colorAtCharacterIndex:effectiveRange:context:),
-                                                                 self,
-                                                                 @selector(ply_colorAtCharacterIndex:effectiveRange:context:),
-                                                                 YES);
+    originalColorAtIndexImp = PLYPoseSwizzle([DVTTextStorage class],
+                                             @selector(colorAtCharacterIndex:effectiveRange:context:),
+                                             self,
+                                             @selector(ply_colorAtCharacterIndex:effectiveRange:context:),
+                                             YES);
 }
 
 #pragma mark - Color overriding
@@ -37,76 +37,100 @@ static IMP originalColorAtCharacterIndexImplementation;
     // Basically, Xcode calls you a given range. It seems to start with the entirety and spiral its way inward.
     // Once given a range, its broken down by the colorAt: method. It then operates on the remainder of the range,
     // effectively iterating from beginning to end using the effectiveRange of the next token and narrowing it down.
-    NSColor *originalColor = originalColorAtCharacterIndexImplementation(self,
-                                                                         @selector(colorAtCharacterIndex:effectiveRange:context:),
-                                                                         index,
-                                                                         effectiveRange,
-                                                                         context);
-
+    NSColor *originalColor = originalColorAtIndexImp(self,
+                                                     @selector(colorAtCharacterIndex:effectiveRange:context:),
+                                                     index,
+                                                     effectiveRange,
+                                                     context);
+    
     if (![[PolychromaticPlugin sharedPlugin] pluginEnabled]) {
         return originalColor;
     }
 
     NSRange newRange = *effectiveRange;
 
+    if ([self ply_isSwiftDocument]) {
+        return [self ply_colorForSwiftCodeRange:newRange context:context fallbackColor:originalColor];
+    }
+    
+    return [self ply_colorForObjCCodeRange:newRange context:context fallbackColor:originalColor];
+}
+
+#pragma mark - Swift
+
+- (BOOL)ply_isSwiftDocument
+{
+    // Check that the Swift language service class exists in the first place.
     static Class swiftLanguageServiceClass = nil;
     static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^
-    {
+    dispatch_once(&onceToken, ^{
         swiftLanguageServiceClass = NSClassFromString(@"IDESourceLanguageServiceSwift");
     });
 
-    /* First account for Swift, and if it isn't, perform the normal Objective-C routine. */
-    if (swiftLanguageServiceClass != nil && [self.languageService isKindOfClass:swiftLanguageServiceClass]) {
-        NSRange swiftEffectiveRange;
-        long long nodeType = [self nodeTypeAtCharacterIndex:newRange.location effectiveRange:&swiftEffectiveRange context:context];
+    // Check that the language service in use is the Swift one.
+    return swiftLanguageServiceClass != nil && [self.languageService isKindOfClass:swiftLanguageServiceClass];
+}
 
-        if (nodeType == [DVTSourceNodeTypes registerNodeTypeNamed:@"xcode.syntax.identifier.variable"]
-            || nodeType == [DVTSourceNodeTypes registerNodeTypeNamed:@"xcode.syntax.identifier.constant"]
-            || nodeType == [DVTSourceNodeTypes registerNodeTypeNamed:@"xcode.syntax.identifier"]) {
-            PLYMockSwift *fauxSwiftService = (PLYMockSwift *)self.languageService;
-            NSRange funcDefinitionRange = [fauxSwiftService methodDefinitionRangeAtIndex:newRange.location];
+- (NSColor *)ply_colorForSwiftCodeRange:(NSRange)range context:(id)context fallbackColor:(NSColor *)fallbackColor
+{
+    NSRange swiftEffectiveRange;
+    long long nodeType = [self nodeTypeAtCharacterIndex:range.location
+                                         effectiveRange:&swiftEffectiveRange
+                                                context:context];
+    
+    if (nodeType == [DVTSourceNodeTypes registerNodeTypeNamed:@"xcode.syntax.identifier.variable"]
+        || nodeType == [DVTSourceNodeTypes registerNodeTypeNamed:@"xcode.syntax.identifier.constant"]
+        || nodeType == [DVTSourceNodeTypes registerNodeTypeNamed:@"xcode.syntax.identifier"]) {
+        PLYMockSwift *fauxSwiftService = (PLYMockSwift *)self.languageService;
+        
+        NSRange funcDefinitionRange = [fauxSwiftService methodDefinitionRangeAtIndex:range.location];
+        if (funcDefinitionRange.location == NSIntegerMax) {
+            NSArray *nameRanges;
+            NSString *name = [self symbolNameAtCharacterIndex:range.location nameRanges:&nameRanges];
 
-            if (funcDefinitionRange.location == NSIntegerMax) {
-                NSArray *nameRanges;
-                NSString *name = [self symbolNameAtCharacterIndex:newRange.location nameRanges:&nameRanges];
-
-                return [[PLYVariableManager sharedManager] colorForVariable:name];
-            }
-        }
-    } else {
-        DVTSourceModelItem *item = [self.sourceModelService sourceModelItemAtCharacterIndex:newRange.location];
-
-        // It's possible for us to simply use the source model, but we want to express fine control based on the node.
-        BOOL isIdentifier = [item ply_isIdentifier];
-        BOOL parentIsMethod = [item.parent ply_isMethod];
-        BOOL inheritsFromPropertyDeclaration = [item ply_inheritsFromNodeOfType:32];
-
-
-        // This is relatively backwards: for some reason, explicitly defined setters and getters in @property are *not*
-        // considered methods/children of methods, whereas the property names themselves are. To combat this, the
-        // following two obscure BOOLs are used to disable the getters/setters and enable the coloring of the property
-        // var names.
-
-        // Disallows getter/setter-attributes from being colored, as their parents are not methods but they inherit from property declarations.
-        BOOL parentIsNotMethodAndDoesNotInheritFromPropertyDeclaration = (!parentIsMethod && !inheritsFromPropertyDeclaration);
-
-        // Ensures property var names are colored as they are considered methods and are within property declarations.
-        BOOL parentIsMethodAndInheritsFromPropertyDeclaration = (parentIsMethod && inheritsFromPropertyDeclaration);
-
-        if (isIdentifier &&
-            (parentIsNotMethodAndDoesNotInheritFromPropertyDeclaration
-             || parentIsMethodAndInheritsFromPropertyDeclaration)) {
-            NSString *string = [self.sourceModelService stringForItem:item];
-
-            if (string) {
-                return [[PLYVariableManager sharedManager] colorForVariable:string];
-            }
+            return [[PLYVariableManager sharedManager] colorForVariable:name];
         }
     }
+    
+    return fallbackColor;
+}
 
-    return originalColor;
+#pragma mark - Objective-C/Generic Code
 
+- (NSColor *)ply_colorForObjCCodeRange:(NSRange)range context:(id)context fallbackColor:(NSColor *)fallbackColor
+{
+    DVTSourceModelItem *item = [self.sourceModelService sourceModelItemAtCharacterIndex:range.location];
+    
+    // It's possible for us to simply use the source model, but we want to express fine control based on the node.
+    BOOL isIdentifier = [item ply_isIdentifier];
+    if (!isIdentifier) {
+        return fallbackColor;
+    }
+    
+    BOOL parentIsMethod = [item.parent ply_isMethod];
+    BOOL inheritsFromPropertyDeclaration = [item ply_inheritsFromNodeOfType:32];
+    
+    // This is relatively backwards: for some reason, explicitly defined setters and getters in @property are *not*
+    // considered methods/children of methods, whereas the property names themselves are. To combat this, the
+    // following two obscure BOOLs are used to disable the getters/setters and enable the coloring of the property
+    // var names.
+    
+    // Disallows getter/setter-attributes from being colored, as their parents are not methods but they inherit from
+    // property declarations.
+    BOOL parentIsNotMethodAndDoesNotInheritFromPropertyDeclaration = !parentIsMethod && !inheritsFromPropertyDeclaration;
+    
+    // Ensures property var names are colored as they are considered methods and are within property declarations.
+    BOOL parentIsMethodAndInheritsFromPropertyDeclaration = (parentIsMethod && inheritsFromPropertyDeclaration);
+    
+    if (parentIsNotMethodAndDoesNotInheritFromPropertyDeclaration || parentIsMethodAndInheritsFromPropertyDeclaration) {
+        NSString *string = [self.sourceModelService stringForItem:item];
+        
+        if (string) {
+            return [[PLYVariableManager sharedManager] colorForVariable:string];
+        }
+    }
+    
+    return fallbackColor;
 }
 
 @end

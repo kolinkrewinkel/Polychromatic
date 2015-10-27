92ec0334cdae32340adb9ae36788013f65b609c1
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-10-27 10:06:51 -0700

    Update copyright dates

--- Polychromatic/Utilities/PLYSwizzling.c
+++ Polychromatic/Utilities/PLYSwizzling.c
@@ -3,7 +3,7 @@
 //  Polychromatic
 //
 //  Created by Kolin Krewinkel on 3/9/14.
-//  Copyright (c) 2014 Kolin Krewinkel. All rights reserved.
+//  Copyright (c) 2015 Kolin Krewinkel. All rights reserved.
 //
 
 #import "PLYSwizzling.h"

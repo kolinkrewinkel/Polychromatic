55b3bf8d45c26e955f39acb5fdd3b4486403892a
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-10-27 23:02:45 -0700

    Prevent flickering when first opening a document by checking if the nodeType is going to be changed.

--- Polychromatic/Categories/DVTTextStorage+PLYHighlightingHook.m
+++ Polychromatic/Categories/DVTTextStorage+PLYHighlightingHook.m
@@ -100,13 +100,14 @@ static IMP originalColorAtIndexImp;
 - (NSColor *)ply_colorForObjCCodeRange:(NSRange)range context:(id)context fallbackColor:(NSColor *)fallbackColor
 {
     DVTSourceModelItem *item = [self.sourceModelService sourceModelItemAtCharacterIndex:range.location];
-    
-    // It's possible for us to simply use the source model, but we want to express fine control based on the node.
-    BOOL isIdentifier = [item ply_isIdentifier];
-    if (!isIdentifier) {
+    if (item.needsAdjustNodeType) {
         return fallbackColor;
     }
-    
+
+    if (![item ply_isIdentifier]) {
+        return fallbackColor;
+    }
+
     BOOL parentIsMethod = [item.parent ply_isMethod];
     BOOL inheritsFromPropertyDeclaration = [item ply_inheritsFromNodeOfType:32];
     
@@ -121,15 +122,14 @@ static IMP originalColorAtIndexImp;
     
     // Ensures property var names are colored as they are considered methods and are within property declarations.
     BOOL parentIsMethodAndInheritsFromPropertyDeclaration = (parentIsMethod && inheritsFromPropertyDeclaration);
-    
+
     if (parentIsNotMethodAndDoesNotInheritFromPropertyDeclaration || parentIsMethodAndInheritsFromPropertyDeclaration) {
         NSString *string = [self.sourceModelService stringForItem:item];
-        
         if (string) {
             return PLYGenerateColor(string);
         }
     }
-    
+
     return fallbackColor;
 }
 
